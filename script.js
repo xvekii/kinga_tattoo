2ddbaac0f7e4f3cc89ce931d8465863f7dab052e
xvekii <vedran.dautanec@gmail.com>
2023-06-24 22:07:14 +0200

    Simplify JS - replace .buttonOne with .hamburger

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-const buttonOne = document.querySelector(".button-one");
+const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
 
-buttonOne.addEventListener("click", () => {
-  const isOpened = buttonOne.getAttribute("aria-expanded"); 
-  if (isOpened === "false") {
-    buttonOne.setAttribute("aria-expanded", "true");
-  } else {
-    buttonOne.setAttribute("aria-expanded", "false");
-  }
-  buttonOne.classList.toggle("active");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   buttonOne.classList.remove("active");
   navMenu.classList.remove("active");
 }));
