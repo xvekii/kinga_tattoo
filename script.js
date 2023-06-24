adcb399f1c80c41d5ca8b27ed7a001a044c272d3
xvekii <vedran.dautanec@gmail.com>
2023-06-24 15:54:41 +0200

    Add eventListener to buttonOne for classList remove

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ buttonOne.addEventListener("click", () => {
   buttonOne.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  buttonOne.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
