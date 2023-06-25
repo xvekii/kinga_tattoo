3f82b366b171d70eeda38aeaf2fa6b8fe0ae7a41
xvekii <vedran.dautanec@gmail.com>
2023-06-25 06:12:33 +0200

    Change buttonOne.classList.remove to hamburger, nav-link to nav__link

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  buttonOne.classList.remove("active");
+document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }));
