bf24ce428821aada256ba4252e4dfbb5a124671c
xvekii <vedran.dautanec@gmail.com>
2023-06-27 12:47:54 +0200

    Edit small logo container, add onscroll opacity functionality for small logo

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click",
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }));
+
+let prevScrollPos = window.scrollY;
+window.onscroll = function() {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollPos > currentScrollPos) {
+    document.querySelector(".small-logo-container").style.opacity = "1";
+    document.querySelector(".small-logo-container").style.pointerEvents = "all";
+  } else {
+    document.querySelector(".small-logo-container").style.opacity = "0";
+    document.querySelector(".small-logo-container").style.pointerEvents = "none";
+  }
+  prevScrollPos = currentScrollPos;
+}
