48ba2e51571ea7cba318a9f6094e815957555ecc
xvekii <vedran.dautanec@gmail.com>
2023-07-07 18:28:09 +0200

    Remove unnecessary comments, remove extra spacing

--- script.js
+++ script.js
@@ -53,13 +53,9 @@ function imgSlide(e) {
 }
 
 
-
-  
 closeModal.addEventListener("click", () => {
   modal.close();
 });
-
-
 
 
 modal.addEventListener("click", (e) => {
@@ -73,29 +69,10 @@ modal.addEventListener("click", (e) => {
 
   } else if (e.target.nodeName === "BUTTON") {
     
-    
-    // let btnAttr = galleryBtn.getAttribute("class");
-    // console.log(btnAttr);
-    
-    // console.log("Alt start: ", currentImageAlt);
-    // currentImageAlt = currentImage.alt;
-    // prevImageNum = currentImageAlt.substring(6);
-    // prevImageNum = Number(prevImageNum);
-    // let counter = 1;
-    // prevImageNum = prevImageNum - counter;
-    // console.log("AltConv", prevImageNum);
-    // currentImage.src = "images/image" + `${prevImageNum}` + ".jpg";
-    // console.log("AltprevImgEnd:", prevImageNum);
-    // console.log(`prev image number last: ${prevImageNum}, currImageSrcEnd: ${currentImage.src}`);
-
-
+    // To be added
+  
   }
-  // } else {
-  //   console.log("error)")
-  // }
 });
-
-
 
 
 // Buttons
@@ -111,81 +88,23 @@ galleryButtons.forEach(function(button) {
     console.log("next works!")
   } else {
     console.log("prev works!");
-    // let currentImageAlt = currentImage.alt;
-    // let prevImageNum = currentImageAlt.substring(6);
-    // prevImageNum = Number(prevImageNum);
-    // let i = 1;
-    // currentImage.src = "images/image" + (prevImageNum - i) + ".jpg";
-    // console.log("prevImg:", prevImageNum);
-    // console.log(`prev image number: ${prevImageNum}, currImageSrc1: ${currentImage.src}`);
+    
+
+
   }
 });
  });
 
-
-
-
-
-  
+// let btnAttr = galleryBtn.getAttribute("class");
+    // console.log(btnAttr);
     
-
-  
-  // let currentImageAlt = currentImage.alt;
-  // console.log(`currentAlt0: ${currentImageAlt}, currImageSrc0: ${currentImage.src}`);
-  // let prevImageNum = currentImageAlt.substring(6);
-  // console.log(prevImageNum);
-  // let newSrc = "images/image" + (prevImageNum - 1) + ".jpg";
-  // currentImage.src = newSrc;
-  // console.log(`prev image number: ${prevImageNum}, currImageSrc1: ${currentImage.src}`);
-
-
-
-
-// button = e.getAttribute;
-//   if (button === "[data-button]") {
-//     alert("Ahoy, Matey!");
-//   }
-
-// function imgChange() {
-//   let currentImage = document.getElementById("current-image");
-//   currentImage.src = "images/image2.jpg";
-//   console.log(currentImage.alt);
-// }
-
-// let currentImage = document.getElementById("#current-image");
-
-
-// let imageThumbs = document.getElementById("gallery-img");
-
-
-// for (let i = 1; i <= 20; i++) {
-//   let thumb = document.createElement("img");
-//   thumb.src = "images/image" + i + ".jpg";
-//   thumb.alt = "Image " + "i";
-
-//   currentImage.addEventListener("click", () => {
-//     currentImage.src = this.src;
-//     currentImage.alt = this.alt;
-//   });
-
-// }
-
-
-
-
-
-
-// small logo disappear on scroll down ==> remove CSS
-
-// let prevScrollPos = window.scrollY;
-// window.onscroll = function() {
-//   let currentScrollPos = window.scrollY;
-//   if ((prevScrollPos > currentScrollPos) & !(prevScrollPos > 100)) {
-//     document.querySelector(".small-logo-container").style.opacity = "1";
-//     document.querySelector(".small-logo-container").style.pointerEvents = "all";
-//   } else {
-//     document.querySelector(".small-logo-container").style.opacity = "0";
-//     document.querySelector(".small-logo-container").style.pointerEvents = "none";
-//   }
-//   prevScrollPos = currentScrollPos;
-// }
+    // console.log("Alt start: ", currentImageAlt);
+    // currentImageAlt = currentImage.alt;
+    // prevImageNum = currentImageAlt.substring(6);
+    // prevImageNum = Number(prevImageNum);
+    // let counter = 1;
+    // prevImageNum = prevImageNum - counter;
+    // console.log("AltConv", prevImageNum);
+    // currentImage.src = "images/image" + `${prevImageNum}` + ".jpg";
+    // console.log("AltprevImgEnd:", prevImageNum);
+    // console.log(`prev image number last: ${prevImageNum}, currImageSrcEnd: ${currentImage.src}`);
